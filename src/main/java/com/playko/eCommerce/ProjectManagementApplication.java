d1a2e676381735d8db5e2721755e8c273bed76e9
Heinner Vega <heinnervega20@gmail.com>
2025-01-22 09:38:29 -0500

    Updated names project

--- src/main/java/com/playko/eCommerce/ProjectManagementApplication.java
+++ src/main/java/com/playko/eCommerce/ProjectManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ECommerceApplication {
+public class ProjectManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ECommerceApplication.class, args);
+		SpringApplication.run(ProjectManagementApplication.class, args);
 	}
 
 }
