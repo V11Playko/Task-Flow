1bd00f7c844fedd4da2db585f85a53a43dfdaebd
Heinner Vega <heinnervega20@gmail.com>
2025-01-27 11:41:36 -0500

    Creating REST controller for authentication handling

---
+++ src/main/java/com/playko/projectManagement/infrastructure/input/rest/AuthRestController.java
@@ -0,0 +1,36 @@
+package com.playko.projectManagement.infrastructure.input.rest;
+
+import com.playko.projectManagement.application.dto.request.LoginRequestDto;
+import com.playko.projectManagement.application.dto.response.JwtTokenResponseDto;
+import com.playko.projectManagement.application.handler.IAuthHandler;
+import com.playko.projectManagement.application.handler.impl.AuthHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/auth")
+@RequiredArgsConstructor
+public class AuthRestController {
+    private final IAuthHandler authHandler;
+
+
+    @Operation(summary = "Login into the system")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login successful", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized for bad credentials", content = @Content)
+    })
+    @PostMapping("/login")
+    public ResponseEntity<JwtTokenResponseDto> loginUser(@RequestBody @Valid LoginRequestDto loginRequestDto) {
+        return new ResponseEntity<>(this.authHandler.loginUser(loginRequestDto), HttpStatus.OK);
+    }
+}
