7600fdee5d7e07dd4d88bebd132020bab3d7c268
Heinner Vega <heinnervega20@gmail.com>
2025-01-29 11:38:35 -0500

    Add DTOs and mappers for user and role

---
+++ src/main/java/com/playko/projectManagement/application/dto/response/RoleResponseDto.java
@@ -0,0 +1,12 @@
+package com.playko.projectManagement.application.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RoleResponseDto {
+    private String name;
+    private String description;
+}
