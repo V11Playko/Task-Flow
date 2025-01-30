a4f6ce2663de9a2229529588a355c6090a8adc58
Heinner Vega <heinnervega20@gmail.com>
2025-01-30 11:34:15 -0500

    Add implementar casos de euso para findUser and findEmail

--- src/main/java/com/playko/projectManagement/application/handler/IUserHandler.java
+++ src/main/java/com/playko/projectManagement/application/handler/IUserHandler.java
@@ -1,7 +1,12 @@
 package com.playko.projectManagement.application.handler;
 
 import com.playko.projectManagement.application.dto.request.UserRequestDto;
+import com.playko.projectManagement.application.dto.response.UserResponseDto;
+
+import java.util.List;
 
 public interface IUserHandler {
+    UserResponseDto findByEmail(String email);
+    List<UserResponseDto> findAllUsers();
     void saveUser(UserRequestDto userRequestDto);
 }
