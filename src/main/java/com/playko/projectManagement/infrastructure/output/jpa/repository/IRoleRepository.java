91d5472fdb62fb73422777c27830c84781b33e7b
Heinner Vega <heinnervega20@gmail.com>
2025-02-04 07:01:12 -0500

    Add DatabaseInitializer

--- src/main/java/com/playko/projectManagement/infrastructure/output/jpa/repository/IRoleRepository.java
+++ src/main/java/com/playko/projectManagement/infrastructure/output/jpa/repository/IRoleRepository.java
@@ -4,6 +4,9 @@ import com.playko.projectManagement.infrastructure.output.jpa.entity.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.management.relation.Role;
+
 @Repository
 public interface IRoleRepository extends JpaRepository<RoleEntity, Long> {
+    RoleEntity findByName(String name);
 }
