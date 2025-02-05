7871af4905f55f7018e53883ddcaa97e6c53da83
Heinner Vega <heinnervega20@gmail.com>
2025-02-05 10:37:34 -0500

    Add DTOs for team creation and user invitation, along with custom exceptions.

---
+++ src/main/java/com/playko/projectManagement/infrastructure/exception/TeamNotFoundException.java
@@ -0,0 +1,7 @@
+package com.playko.projectManagement.infrastructure.exception;
+
+public class TeamNotFoundException extends RuntimeException{
+    public TeamNotFoundException() {
+        super();
+    }
+}
