886c24bc4acb678268021da554de0106877551cf
Heinner Vega <heinnervega20@gmail.com>
2025-02-05 10:36:41 -0500

    Add Implement use case logic for team creation and adding users to teams.

---
+++ src/main/java/com/playko/projectManagement/application/handler/impl/TeamHandler.java
@@ -0,0 +1,28 @@
+package com.playko.projectManagement.application.handler.impl;
+
+import com.playko.projectManagement.application.dto.request.TeamRequestDto;
+import com.playko.projectManagement.application.handler.ITeamHandler;
+import com.playko.projectManagement.application.mapper.request.ITeamRequestMapper;
+import com.playko.projectManagement.domain.api.ITeamServicePort;
+import com.playko.projectManagement.domain.model.TeamModel;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class TeamHandler implements ITeamHandler {
+    private final ITeamServicePort teamServicePort;
+    private final ITeamRequestMapper teamRequestMapper;
+    @Override
+    public void saveTeam(TeamRequestDto teamRequestDto) {
+        TeamModel teamModel = teamRequestMapper.toTeamRequest(teamRequestDto);
+        teamServicePort.saveTeam(teamModel);
+    }
+
+    @Override
+    public void addUserToTeam(Long teamId, String emailUser) {
+        teamServicePort.addUserToTeam(teamId, emailUser);
+    }
+}
