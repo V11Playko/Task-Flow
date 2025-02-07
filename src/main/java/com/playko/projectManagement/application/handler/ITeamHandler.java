1e5dfbf5298800d40922600ef93f4b2114392dba
Heinner Vega <heinnervega20@gmail.com>
2025-02-07 11:42:21 -0500

    Add remove user from team feature - Add removeUserFromTeam method to service and persistence ports - Create RemoveUserFromTeamRequestDto

--- src/main/java/com/playko/projectManagement/application/handler/ITeamHandler.java
+++ src/main/java/com/playko/projectManagement/application/handler/ITeamHandler.java
@@ -5,4 +5,5 @@ import com.playko.projectManagement.application.dto.request.TeamRequestDto;
 public interface ITeamHandler {
     void saveTeam(TeamRequestDto teamRequestDto);
     void addUserToTeam(Long teamId, String emailUser);
+    void removeUserFromTeam(Long teamId, String emailUser);
 }
