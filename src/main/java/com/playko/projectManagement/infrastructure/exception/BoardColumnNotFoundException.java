9b9b7773898fa19ac66b9b326e27293be3a5b279
Heinner Vega <heinnervega20@gmail.com>
2025-02-11 12:20:51 -0500

    Add two new exceptions

---
+++ src/main/java/com/playko/projectManagement/infrastructure/exception/BoardColumnNotFoundException.java
@@ -0,0 +1,7 @@
+package com.playko.projectManagement.infrastructure.exception;
+
+public class BoardColumnNotFoundException extends RuntimeException{
+    public BoardColumnNotFoundException() {
+        super();
+    }
+}
