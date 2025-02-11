8f659769b93cdab2a0cb75dc2d0c1e2aaf2e66f2
Heinner Vega <heinnervega20@gmail.com>
2025-02-11 12:48:40 -0500

    Updated taskHandler, I forgot to add the content to the method

--- src/main/java/com/playko/projectManagement/application/handler/impl/TaskHandler.java
+++ src/main/java/com/playko/projectManagement/application/handler/impl/TaskHandler.java
@@ -4,6 +4,7 @@ import com.playko.projectManagement.application.dto.request.TaskRequestDto;
 import com.playko.projectManagement.application.handler.ITaskHandler;
 import com.playko.projectManagement.application.mapper.request.ITaskRequestMapper;
 import com.playko.projectManagement.domain.api.ITaskServicePort;
+import com.playko.projectManagement.domain.model.TaskModel;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ public class TaskHandler implements ITaskHandler {
     private final ITaskRequestMapper taskRequestMapper;
     @Override
     public void saveTask(TaskRequestDto taskRequestDto) {
-
+        TaskModel taskModel = taskRequestMapper.toModel(taskRequestDto);
+        taskServicePort.saveTask(taskModel);
     }
 }
