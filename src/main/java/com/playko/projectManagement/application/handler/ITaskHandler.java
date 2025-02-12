3b62f6d3277ad63a47931693ff2f61227e6a9313
Heinner Vega <heinnervega20@gmail.com>
2025-02-12 11:58:52 -0500

    Implement task assignment logic in TaskHandler - Add assignTaskToUser method in TaskHandler - Converts DTO to model before passing to service layer

--- src/main/java/com/playko/projectManagement/application/handler/ITaskHandler.java
+++ src/main/java/com/playko/projectManagement/application/handler/ITaskHandler.java
@@ -1,7 +1,9 @@
 package com.playko.projectManagement.application.handler;
 
+import com.playko.projectManagement.application.dto.request.TaskAssignmentRequestDto;
 import com.playko.projectManagement.application.dto.request.TaskRequestDto;
 
 public interface ITaskHandler {
     void saveTask(TaskRequestDto taskRequestDto);
+    void assignTaskToUser(TaskAssignmentRequestDto taskAssignmentRequestDto);
 }
