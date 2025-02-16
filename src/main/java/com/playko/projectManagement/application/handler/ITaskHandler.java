66ea43a4493ddb0176b038b9ee223fdb57994bb9
Heinner Vega <heinnervega20@gmail.com>
2025-02-15 21:37:32 -0500

    File paths have been updated

--- src/main/java/com/playko/projectManagement/application/handler/ITaskHandler.java
+++ src/main/java/com/playko/projectManagement/application/handler/ITaskHandler.java
@@ -1,8 +1,8 @@
 package com.playko.projectManagement.application.handler;
 
-import com.playko.projectManagement.application.dto.request.TaskAssignmentRequestDto;
-import com.playko.projectManagement.application.dto.request.TaskReassignmentRequestDto;
-import com.playko.projectManagement.application.dto.request.TaskRequestDto;
+import com.playko.projectManagement.application.dto.request.task.TaskAssignmentRequestDto;
+import com.playko.projectManagement.application.dto.request.task.TaskReassignmentRequestDto;
+import com.playko.projectManagement.application.dto.request.task.TaskRequestDto;
 
 public interface ITaskHandler {
     void saveTask(TaskRequestDto taskRequestDto);
