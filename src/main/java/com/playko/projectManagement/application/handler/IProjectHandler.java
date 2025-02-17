917dd43240721770d72baf9529fb28ee434d852d
Heinner Vega <heinnervega20@gmail.com>
2025-02-17 11:09:19 -0500

    Add interfaces for updateProjectDeadline method

--- src/main/java/com/playko/projectManagement/application/handler/IProjectHandler.java
+++ src/main/java/com/playko/projectManagement/application/handler/IProjectHandler.java
@@ -1,7 +1,12 @@
 package com.playko.projectManagement.application.handler;
 
+import com.playko.projectManagement.application.dto.request.ProjectDeadlineRequestDto;
 import com.playko.projectManagement.application.dto.request.ProjectRequestDto;
+
+import java.time.LocalDate;
 
 public interface IProjectHandler {
     void createProject(ProjectRequestDto projectRequestDto);
+    void updateProjectDeadline(ProjectDeadlineRequestDto projectDeadlineRequestDto);
+
 }
