b718e334814d713589ef6bbb4f9cebe4002e2a41
Heinner Vega <heinnervega20@gmail.com>
2025-02-17 11:10:37 -0500

    Add ProjectDeadline request dto

---
+++ src/main/java/com/playko/projectManagement/application/dto/request/ProjectDeadlineRequestDto.java
@@ -0,0 +1,15 @@
+package com.playko.projectManagement.application.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ProjectDeadlineRequestDto {
+    private Long projectId;
+    private LocalDate deadline;
+}
