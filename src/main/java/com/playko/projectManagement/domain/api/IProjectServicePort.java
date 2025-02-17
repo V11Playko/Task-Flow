4ebcea52ca181eea2a4d690c01957f5f1d058ad5
Heinner Vega <heinnervega20@gmail.com>
2025-02-17 11:09:19 -0500

    Add interfaces for updateProjectDeadline method

--- src/main/java/com/playko/projectManagement/domain/api/IProjectServicePort.java
+++ src/main/java/com/playko/projectManagement/domain/api/IProjectServicePort.java
@@ -2,6 +2,10 @@ package com.playko.projectManagement.domain.api;
 
 import com.playko.projectManagement.domain.model.ProjectModel;
 
+import java.time.LocalDate;
+
 public interface IProjectServicePort {
     void createProject(ProjectModel projectModel);
+    void updateProjectDeadline(Long projectId, LocalDate deadline);
+
 }
