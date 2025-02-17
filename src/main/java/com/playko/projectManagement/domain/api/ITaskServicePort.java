fe59a116b3ba365ceeb6aea5a29456f94d4dddb7
Heinner Vega <heinnervega20@gmail.com>
2025-02-17 11:40:15 -0500

    Add interfaces for updateTaskDeadline method

--- src/main/java/com/playko/projectManagement/domain/api/ITaskServicePort.java
+++ src/main/java/com/playko/projectManagement/domain/api/ITaskServicePort.java
@@ -2,8 +2,11 @@ package com.playko.projectManagement.domain.api;
 
 import com.playko.projectManagement.domain.model.TaskModel;
 
+import java.time.LocalDate;
+
 public interface ITaskServicePort {
     void saveTask(TaskModel taskModel);
     void assignTaskToUser(Long taskId, Long userId);
     void reassignTask(Long taskId, Long newUserId);
+    void updateTaskDeadline(Long taskId, LocalDate deadline);
 }
