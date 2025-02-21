bef248ec3fd1b629ab2281d278cb24fc2981f332
Heinner Vega <heinnervega20@gmail.com>
2025-02-21 12:01:12 -0500

    Implement response mapping in TaskHandler and update TaskHandler interface to include response mapping

--- src/main/java/com/playko/projectManagement/application/handler/ITaskHandler.java
+++ src/main/java/com/playko/projectManagement/application/handler/ITaskHandler.java
@@ -4,9 +4,13 @@ import com.playko.projectManagement.application.dto.request.task.TaskAssignmentR
 import com.playko.projectManagement.application.dto.request.task.TaskDeadlineRequestDto;
 import com.playko.projectManagement.application.dto.request.task.TaskReassignmentRequestDto;
 import com.playko.projectManagement.application.dto.request.task.TaskRequestDto;
+import com.playko.projectManagement.application.dto.response.TaskResponseDto;
 import com.playko.projectManagement.shared.enums.TaskState;
 
+import java.util.List;
+
 public interface ITaskHandler {
+    List<TaskResponseDto> getTasksByUserEmail();
     void saveTask(TaskRequestDto taskRequestDto);
     void assignTaskToUser(TaskAssignmentRequestDto taskAssignmentRequestDto);
     void reassignTask(TaskReassignmentRequestDto taskReassignmentRequestDto);
