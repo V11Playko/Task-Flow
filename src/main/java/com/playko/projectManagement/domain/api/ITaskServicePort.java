a1b5d6812d7e7546da9e64a26a9df52371f68f1c
Heinner Vega <heinnervega20@gmail.com>
2025-02-21 12:01:59 -0500

    Update TaskServicePort to support new response structure

--- src/main/java/com/playko/projectManagement/domain/api/ITaskServicePort.java
+++ src/main/java/com/playko/projectManagement/domain/api/ITaskServicePort.java
@@ -4,8 +4,10 @@ import com.playko.projectManagement.domain.model.TaskModel;
 import com.playko.projectManagement.shared.enums.TaskState;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface ITaskServicePort {
+    List<TaskModel> getTasksByUserEmail();
     void saveTask(TaskModel taskModel);
     void assignTaskToUser(Long taskId, Long userId);
     void reassignTask(Long taskId, Long newUserId);
