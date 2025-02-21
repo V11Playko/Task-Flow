439edc147bbad300aea3f731c36458a88740c998
Heinner Vega <heinnervega20@gmail.com>
2025-02-21 12:05:11 -0500

    Update TaskEntityMapper and TaskRepository for response handling

--- src/main/java/com/playko/projectManagement/infrastructure/output/jpa/repository/ITaskRepository.java
+++ src/main/java/com/playko/projectManagement/infrastructure/output/jpa/repository/ITaskRepository.java
@@ -1,9 +1,13 @@
 package com.playko.projectManagement.infrastructure.output.jpa.repository;
 
 import com.playko.projectManagement.infrastructure.output.jpa.entity.TaskEntity;
+import com.playko.projectManagement.infrastructure.output.jpa.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ITaskRepository extends JpaRepository<TaskEntity, Long> {
+    List<TaskEntity> findByAssignedUser(UserEntity user);
 }
