9b74586afc5b5577717b71d890184ba7a40f26a2
Heinner Vega <heinnervega20@gmail.com>
2025-02-24 09:48:08 -0500

    Implement getCommentsByTask in CommentHandler

--- src/main/java/com/playko/projectManagement/application/handler/impl/CommentHandler.java
+++ src/main/java/com/playko/projectManagement/application/handler/impl/CommentHandler.java
@@ -1,11 +1,15 @@
 package com.playko.projectManagement.application.handler.impl;
 
 import com.playko.projectManagement.application.dto.request.CommentRequestDto;
+import com.playko.projectManagement.application.dto.response.CommentResponseDto;
 import com.playko.projectManagement.application.handler.ICommentHandler;
 import com.playko.projectManagement.application.mapper.request.ICommentRequestMapper;
 import com.playko.projectManagement.domain.api.ICommentServicePort;
 import com.playko.projectManagement.domain.model.CommentModel;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class CommentHandler implements ICommentHandler {
@@ -16,5 +20,17 @@ public class CommentHandler implements ICommentHandler {
     public void addComment(CommentRequestDto commentRequestDto) {
         CommentModel commentModel = commentRequestMapper.toModel(commentRequestDto);
         commentServicePort.addComment(commentModel);
+    }
+
+    @Override
+    public List<CommentResponseDto> getCommentsByTask(Long taskId) {
+        return commentServicePort.getCommentsByTask(taskId)
+                .stream()
+                .map(comment -> new CommentResponseDto(
+                        comment.getId(),
+                        comment.getContent(),
+                        comment.getCreatedAt(),
+                        comment.getUser().getName()))
+                .collect(Collectors.toList());
     }
 }
