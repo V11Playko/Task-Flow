6cd6847e35546d135dd464c49415f58ec32a71a6
Heinner Vega <heinnervega20@gmail.com>
2025-02-24 09:47:58 -0500

    Update ICommentHandler to include getCommentsByTask

--- src/main/java/com/playko/projectManagement/application/handler/ICommentHandler.java
+++ src/main/java/com/playko/projectManagement/application/handler/ICommentHandler.java
@@ -1,7 +1,11 @@
 package com.playko.projectManagement.application.handler;
 
 import com.playko.projectManagement.application.dto.request.CommentRequestDto;
+import com.playko.projectManagement.application.dto.response.CommentResponseDto;
+
+import java.util.List;
 
 public interface ICommentHandler {
     void addComment(CommentRequestDto commentRequestDto);
+    List<CommentResponseDto> getCommentsByTask(Long taskId);
 }
