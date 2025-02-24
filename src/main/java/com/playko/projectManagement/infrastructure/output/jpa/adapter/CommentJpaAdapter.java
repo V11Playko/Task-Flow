9b54bfe18e7a4cf6740957c6503a86f51aad5db2
Heinner Vega <heinnervega20@gmail.com>
2025-02-24 09:49:34 -0500

    Implement getCommentsByTask in CommentJpaAdapter

--- src/main/java/com/playko/projectManagement/infrastructure/output/jpa/adapter/CommentJpaAdapter.java
+++ src/main/java/com/playko/projectManagement/infrastructure/output/jpa/adapter/CommentJpaAdapter.java
@@ -2,15 +2,19 @@ package com.playko.projectManagement.infrastructure.output.jpa.adapter;
 
 import com.playko.projectManagement.domain.model.CommentModel;
 import com.playko.projectManagement.domain.spi.ICommentPersistencePort;
+import com.playko.projectManagement.infrastructure.exception.TaskNotFoundException;
+import com.playko.projectManagement.infrastructure.exception.UserNotFoundException;
 import com.playko.projectManagement.infrastructure.output.jpa.entity.CommentEntity;
 import com.playko.projectManagement.infrastructure.output.jpa.entity.TaskEntity;
 import com.playko.projectManagement.infrastructure.output.jpa.entity.UserEntity;
+import com.playko.projectManagement.infrastructure.output.jpa.mapper.ICommentEntityMapper;
 import com.playko.projectManagement.infrastructure.output.jpa.repository.ICommentRepository;
 import com.playko.projectManagement.infrastructure.output.jpa.repository.ITaskRepository;
 import com.playko.projectManagement.infrastructure.output.jpa.repository.IUserRepository;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class CommentJpaAdapter implements ICommentPersistencePort {
@@ -21,9 +25,9 @@ public class CommentJpaAdapter implements ICommentPersistencePort {
 
     @Override
     public void addComment(CommentModel commentModel) {
-        TaskEntity task = taskRepository.findById(commentModel.getTaskId())
+        TaskEntity task = taskRepository.findById(commentModel.getTask().getId())
                 .orElseThrow(TaskNotFoundException::new);
-        UserEntity user = userRepository.findById(commentModel.getUserId())
+        UserEntity user = userRepository.findById(commentModel.getUser().getId())
                 .orElseThrow(UserNotFoundException::new);
 
         CommentEntity commentEntity = commentEntityMapper.toEntity(commentModel);
@@ -32,6 +36,11 @@ public class CommentJpaAdapter implements ICommentPersistencePort {
         commentEntity.setCreatedAt(LocalDateTime.now());
 
         commentRepository.save(commentEntity);
+    }
+
+    @Override
+    public List<CommentModel> getCommentsByTask(Long taskId) {
+        return commentEntityMapper.toModelList(commentRepository.findByTaskId(taskId));
     }
 
 }
