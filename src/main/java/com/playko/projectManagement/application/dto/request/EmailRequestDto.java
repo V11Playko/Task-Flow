a2953c7700c13228739b22d2dc6c58da1922c1c4
Heinner Vega <heinnervega20@gmail.com>
2025-02-25 11:29:11 -0500

    Add EmailRequestDto for email handling

---
+++ src/main/java/com/playko/projectManagement/application/dto/request/EmailRequestDto.java
@@ -0,0 +1,17 @@
+package com.playko.projectManagement.application.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class EmailRequestDto {
+
+    private String destinatario;
+    private String asunto;
+    private String mensaje;
+}
