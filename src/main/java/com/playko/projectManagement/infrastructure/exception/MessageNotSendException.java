f2be2ab5002817a060cdf991d33fbe8d038cf812
Heinner Vega <heinnervega20@gmail.com>
2025-02-26 09:16:32 -0500

    Add MessageNotSendException for email failures

---
+++ src/main/java/com/playko/projectManagement/infrastructure/exception/MessageNotSendException.java
@@ -0,0 +1,7 @@
+package com.playko.projectManagement.infrastructure.exception;
+
+public class MessageNotSendException extends RuntimeException{
+    public MessageNotSendException() {
+        super();
+    }
+}
