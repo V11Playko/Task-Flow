7c90011f0a10d52b067850edfe141d6701718a20
Heinner Vega <heinnervega20@gmail.com>
2025-02-26 09:16:44 -0500

    Implement EmailJpaAdapter to handle email persistence

---
+++ src/main/java/com/playko/projectManagement/infrastructure/output/jpa/adapter/EmailJpaAdapter.java
@@ -0,0 +1,40 @@
+package com.playko.projectManagement.infrastructure.output.jpa.adapter;
+
+import com.playko.projectManagement.application.dto.request.EmailRequestDto;
+import com.playko.projectManagement.domain.spi.IEmailPersistencePort;
+import com.playko.projectManagement.infrastructure.exception.MessageNotSendException;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@RequiredArgsConstructor
+public class EmailJpaAdapter implements IEmailPersistencePort {
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
+
+    @Override
+    @Transactional
+    public void sendEmail(EmailRequestDto email) {
+        try{
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo(email.getDestinatario());
+            helper.setSubject(email.getAsunto());
+
+            Context context = new Context();
+            context.setVariable("message", email.getMensaje());
+            String contenidoHtml = templateEngine.process("email", context);
+
+            helper.setText(contenidoHtml, true);
+
+            javaMailSender.send(message);
+        }catch (Exception e){
+            throw new MessageNotSendException();
+        }
+    }
+}
