6c1d34f95ed36bec9c90f05b3d06091bae726511
Heinner Vega <heinnervega20@gmail.com>
2025-02-27 10:44:31 -0500

    Modify mappers for request and entity transformations

--- src/main/java/com/playko/projectManagement/application/mapper/request/ITaskRequestMapper.java
+++ src/main/java/com/playko/projectManagement/application/mapper/request/ITaskRequestMapper.java
@@ -4,12 +4,18 @@ import com.playko.projectManagement.application.dto.request.task.TaskAssignmentR
 import com.playko.projectManagement.application.dto.request.task.TaskRequestDto;
 import com.playko.projectManagement.application.dto.response.TaskResponseDto;
 import com.playko.projectManagement.domain.model.TaskModel;
+import com.playko.projectManagement.domain.model.UserModel;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
@@ -23,8 +29,16 @@ public interface ITaskRequestMapper {
     TaskModel toModel(TaskRequestDto taskRequestDto);
 
     @Mapping(target = "id", source = "taskId")
-    @Mapping(target = "assignedUserId", source = "userId")
+    @Mapping(target = "assignedUserId.id", source = "userId")
     TaskModel taskAssignmentToModel(TaskAssignmentRequestDto assignmentRequestDto);
 
+    @Mapping(target = "assignedUserId", source = "assignedUserId", qualifiedByName = "mapUserModelToLong")
+    TaskResponseDto toDto(TaskModel taskModel);
+
     List<TaskResponseDto> toDtoList(List<TaskModel> taskModels);
+
+    @Named("mapUserModelToLong")
+    default Long mapUserModelToLong(UserModel user) {
+        return (user != null) ? user.getId() : null;
+    }
 }
