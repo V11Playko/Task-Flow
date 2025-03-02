c6c9465785bc27faebcb1b67b0bb6bf37de729ea
Heinner Vega <heinnervega20@gmail.com>
2025-03-02 12:58:33 -0500

    Handle BoardNotFoundException in ControllerAdvisor

---
+++ src/main/java/com/playko/projectManagement/infrastructure/exception/BoardNotFoundException.java
@@ -0,0 +1,7 @@
+package com.playko.projectManagement.infrastructure.exception;
+
+public class BoardNotFoundException extends RuntimeException{
+    public BoardNotFoundException() {
+        super();
+    }
+}
