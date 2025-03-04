3aadca81ad31b23bf046f07b04f9e34ce140e366
Heinner Vega <heinnervega20@gmail.com>
2025-03-04 09:01:22 -0500

    Add saveBoard logic to BoardJpaAdapter and entity mapper

--- src/main/java/com/playko/projectManagement/infrastructure/output/jpa/adapter/BoardJpaAdapter.java
+++ src/main/java/com/playko/projectManagement/infrastructure/output/jpa/adapter/BoardJpaAdapter.java
@@ -4,13 +4,16 @@ import com.playko.projectManagement.domain.model.BoardModel;
 import com.playko.projectManagement.domain.spi.IBoardPersistencePort;
 import com.playko.projectManagement.infrastructure.exception.BoardColumnNotFoundException;
 import com.playko.projectManagement.infrastructure.exception.BoardNotFoundException;
+import com.playko.projectManagement.infrastructure.exception.ProjectNotFoundException;
 import com.playko.projectManagement.infrastructure.exception.TaskNotFoundException;
 import com.playko.projectManagement.infrastructure.output.jpa.entity.BoardColumnEntity;
 import com.playko.projectManagement.infrastructure.output.jpa.entity.BoardEntity;
+import com.playko.projectManagement.infrastructure.output.jpa.entity.ProjectEntity;
 import com.playko.projectManagement.infrastructure.output.jpa.entity.TaskEntity;
 import com.playko.projectManagement.infrastructure.output.jpa.mapper.IBoardEntityMapper;
 import com.playko.projectManagement.infrastructure.output.jpa.repository.IBoardColumnRepository;
 import com.playko.projectManagement.infrastructure.output.jpa.repository.IBoardRepository;
+import com.playko.projectManagement.infrastructure.output.jpa.repository.IProjectRepository;
 import com.playko.projectManagement.infrastructure.output.jpa.repository.ITaskRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +23,7 @@ public class BoardJpaAdapter implements IBoardPersistencePort {
     private final IBoardEntityMapper boardEntityMapper;
     private final ITaskRepository taskRepository;
     private final IBoardColumnRepository boardColumnRepository;
+    private final IProjectRepository projectRepository;
 
     @Override
     public BoardModel getBoardById(Long boardId) {
@@ -37,5 +41,16 @@ public class BoardJpaAdapter implements IBoardPersistencePort {
 
         taskEntity.setBoardColumn(boardColumn);
         taskRepository.save(taskEntity);
+    }
+
+    @Override
+    public void saveBoard(BoardModel boardModel) {
+        ProjectEntity project = projectRepository.findById(boardModel.getProject().getId())
+                .orElseThrow(ProjectNotFoundException::new);
+
+        BoardEntity board = boardEntityMapper.toEntity(boardModel);
+        board.setProject(project);
+
+        boardRepository.save(board);
     }
 }
