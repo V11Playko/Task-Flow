51c35691ec06d532dab46705adec1253bfcdf31b
Heinner Vega <heinnervega20@gmail.com>
2025-03-07 11:43:36 -0500

    Define methods for retrieving project statistics in interfaces

--- src/main/java/com/playko/projectManagement/application/handler/IProjectHandler.java
+++ src/main/java/com/playko/projectManagement/application/handler/IProjectHandler.java
@@ -2,6 +2,7 @@ package com.playko.projectManagement.application.handler;
 
 import com.playko.projectManagement.application.dto.request.ProjectDeadlineRequestDto;
 import com.playko.projectManagement.application.dto.request.ProjectRequestDto;
+import com.playko.projectManagement.application.dto.response.ProjectStatsDto;
 
 import java.time.LocalDate;
 
@@ -9,4 +10,5 @@ public interface IProjectHandler {
     void createProject(ProjectRequestDto projectRequestDto);
     void updateProjectDeadline(ProjectDeadlineRequestDto projectDeadlineRequestDto);
     void archiveProject(Long projectId);
+    ProjectStatsDto getProjectStats(Long projectId);
 }
