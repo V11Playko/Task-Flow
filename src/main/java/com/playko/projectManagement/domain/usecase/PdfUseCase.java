d172890786e0847700797bf5e2a70313dc491ddc
Heinner Vega <heinnervega20@gmail.com>
2025-03-10 11:36:51 -0500

    Implement PDF generation logic in PdfUseCase

---
+++ src/main/java/com/playko/projectManagement/domain/usecase/PdfUseCase.java
@@ -0,0 +1,20 @@
+package com.playko.projectManagement.domain.usecase;
+
+import com.playko.projectManagement.domain.api.IPdfServicePort;
+import com.playko.projectManagement.domain.spi.IPdfPersistencePort;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class PdfUseCase implements IPdfServicePort {
+    private final IPdfPersistencePort pdfPersistencePort;
+
+    public PdfUseCase(IPdfPersistencePort pdfPersistencePort) {
+        this.pdfPersistencePort = pdfPersistencePort;
+    }
+
+    @Override
+    public ByteArrayOutputStream generatePdf(Long projectId) throws IOException {
+        return pdfPersistencePort.generatePdf(projectId);
+    }
+}
