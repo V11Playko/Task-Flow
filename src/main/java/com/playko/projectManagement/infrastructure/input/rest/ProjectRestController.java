2a51184a7afd2938bbead8449f0b91a99afac299
Heinner Vega <heinnervega20@gmail.com>
2025-03-10 11:38:29 -0500

    Integrate PDF report generation into ProjectRestController

--- src/main/java/com/playko/projectManagement/infrastructure/input/rest/ProjectRestController.java
+++ src/main/java/com/playko/projectManagement/infrastructure/input/rest/ProjectRestController.java
@@ -10,7 +10,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/project")
