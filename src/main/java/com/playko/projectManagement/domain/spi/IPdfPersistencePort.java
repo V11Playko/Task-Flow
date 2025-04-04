e1e91d59599e1583d0e19bd801b631e7dd820d9c
Heinner Vega <heinnervega20@gmail.com>
2025-04-04 09:34:41 -0500

    Definir interfaces para generación de reportes en PDF

--- src/main/java/com/playko/projectManagement/domain/spi/IPdfPersistencePort.java
+++ src/main/java/com/playko/projectManagement/domain/spi/IPdfPersistencePort.java
@@ -1,8 +1,12 @@
 package com.playko.projectManagement.domain.spi;
 
+import com.playko.projectManagement.application.dto.request.team.TeamPerformanceReportDto;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public interface IPdfPersistencePort {
     ByteArrayOutputStream generatePdf(Long projectId) throws IOException;
+
+    byte[] generatePerformanceReportPdf(Long teamId) throws IOException;
 }
