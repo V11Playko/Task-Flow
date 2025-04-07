898ddc2267074321635ac5958b3b69bdde874ae5
Heinner Vega <heinnervega20@gmail.com>
2025-04-07 11:04:21 -0500

    Define método en TaskServicePort para sincronizar con calendario

--- src/main/java/com/playko/projectManagement/domain/api/ITaskServicePort.java
+++ src/main/java/com/playko/projectManagement/domain/api/ITaskServicePort.java
@@ -5,7 +5,6 @@ import com.playko.projectManagement.shared.enums.TaskPriority;
 import com.playko.projectManagement.shared.enums.TaskState;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -21,5 +20,6 @@ public interface ITaskServicePort {
     void deleteTask(Long taskId);
     List<TaskModel> searchTasksByKeyword(String keyword);
     String storeFile(MultipartFile file) throws Exception;
+    String generateIcsForTasks();
 
 }
