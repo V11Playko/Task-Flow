9f3aaf7fc1219f5153360caab25b98d5363bb60e
Heinner Vega <heinnervega20@gmail.com>
2025-04-11 14:48:45 -0500

    Add new InvalidRestricion excepction

---
+++ src/main/java/com/playko/projectManagement/infrastructure/exception/InvalidRestrictionException.java
@@ -0,0 +1,7 @@
+package com.playko.projectManagement.infrastructure.exception;
+
+public class InvalidRestrictionException extends RuntimeException {
+    public InvalidRestrictionException() {
+        super();
+    }
+}
