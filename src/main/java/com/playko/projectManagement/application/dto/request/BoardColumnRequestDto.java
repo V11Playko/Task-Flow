b5fd48c15857d7eec8d48f2ee4713a939357095e
Heinner Vega <heinnervega20@gmail.com>
2025-04-14 08:59:10 -0500

    Add validaciones a BoardColumnRequestDto, EmailRequestDto y BoardRequestDto

--- src/main/java/com/playko/projectManagement/application/dto/request/BoardColumnRequestDto.java
+++ src/main/java/com/playko/projectManagement/application/dto/request/BoardColumnRequestDto.java
@@ -1,5 +1,7 @@
 package com.playko.projectManagement.application.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,5 +10,8 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class BoardColumnRequestDto {
+
+    @NotBlank(message = "El nombre de la columna no puede estar vacío.")
+    @Size(max = 50, message = "El nombre de la columna no puede superar los 50 caracteres.")
     private String name;
 }
