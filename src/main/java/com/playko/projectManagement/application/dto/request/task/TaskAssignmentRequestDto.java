53b8507876676acce8ea548734d312c37592143b
Heinner Vega <heinnervega20@gmail.com>
2025-04-15 16:10:03 -0500

    Add validaciones a los DTOs relacionados con tareas

--- src/main/java/com/playko/projectManagement/application/dto/request/task/TaskAssignmentRequestDto.java
+++ src/main/java/com/playko/projectManagement/application/dto/request/task/TaskAssignmentRequestDto.java
@@ -4,10 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import jakarta.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class TaskAssignmentRequestDto {
+
+    @NotNull(message = "El ID de la tarea no puede ser nulo")
     private Long taskId;
+
+    @NotNull(message = "El ID del usuario no puede ser nulo")
     private Long userId;
 }
