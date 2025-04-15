f0513cad2d5cb56d0591c91f332ae16f4ca78918
Heinner Vega <heinnervega20@gmail.com>
2025-04-15 16:06:41 -0500

    Add validaciones a TeamEmailRequestDto, AddUserToTeamRequestDt y RemoveUserFromTeamRequestDto

--- src/main/java/com/playko/projectManagement/application/dto/request/team/AddUserToTeamRequestDto.java
+++ src/main/java/com/playko/projectManagement/application/dto/request/team/AddUserToTeamRequestDto.java
@@ -5,11 +5,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddUserToTeamRequestDto {
+
+    @NotNull(message = "El ID del equipo es obligatorio")
     private Long teamId;
+
+    @NotBlank(message = "El correo del usuario es obligatorio")
+    @Email(message = "Debe proporcionar un correo electrónico válido")
     private String emailUser;
 }
--- src/main/java/com/playko/projectManagement/application/dto/request/team/TeamEmailRequestDto.java
+++ src/main/java/com/playko/projectManagement/application/dto/request/team/TeamEmailRequestDto.java
@@ -4,12 +4,27 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class TeamEmailRequestDto {
+
+    @NotNull(message = "El ID del equipo es obligatorio")
     private Long teamId;
+
+    @NotBlank(message = "El asunto es obligatorio")
+    @Size(max = 100, message = "El asunto no puede tener más de 100 caracteres")
     private String subject;
+
+    @NotBlank(message = "El mensaje es obligatorio")
+    @Size(max = 1000, message = "El mensaje no puede tener más de 1000 caracteres")
     private String message;
+
+    @NotBlank(message = "El remitente es obligatorio")
+    @Size(max = 100, message = "El remitente no puede tener más de 100 caracteres")
     private String remitente;
 }
