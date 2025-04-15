9330111a12a138990e5fafb6d5bd680a1dbeab50
Heinner Vega <heinnervega20@gmail.com>
2025-04-15 16:20:54 -0500

    Analyzed with SonarLint

--- src/main/java/com/playko/projectManagement/application/dto/request/EmailRequestDto.java
+++ src/main/java/com/playko/projectManagement/application/dto/request/EmailRequestDto.java
@@ -8,11 +8,6 @@ import lombok.Setter;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
