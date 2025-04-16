82d81e03e4bba54aca61d279cfe4d35b85b99025
Heinner Vega <heinnervega20@gmail.com>
2025-04-16 09:34:01 -0500

    Fix IUserEntityMapper

--- src/main/java/com/playko/projectManagement/infrastructure/output/jpa/mapper/IUserEntityMapper.java
+++ src/main/java/com/playko/projectManagement/infrastructure/output/jpa/mapper/IUserEntityMapper.java
@@ -3,6 +3,7 @@ package com.playko.projectManagement.infrastructure.output.jpa.mapper;
 import com.playko.projectManagement.domain.model.UserModel;
 import com.playko.projectManagement.infrastructure.output.jpa.entity.UserEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
@@ -12,6 +13,9 @@ import java.util.List;
         unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface IUserEntityMapper {
     UserEntity toEntity(UserModel userModel);
+    @Mapping(target = "roleModel.name", source = "roleEntity.name")
+    @Mapping(target = "roleModel.description", source = "roleEntity.description")
+    @Mapping(target = "team.name", source = "team.name")
     UserModel toUserModel(UserEntity userEntity);
 
     List<UserModel> toUserModelList(List<UserEntity> userEntities);
