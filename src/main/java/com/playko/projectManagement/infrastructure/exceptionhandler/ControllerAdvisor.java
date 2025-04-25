6169eab6b2ecad5311de99d10e43c3ec29044d70
Heinner Vega <heinnervega20@gmail.com>
2025-04-25 08:24:34 -0500

    Delete limpiar imports no usados de Exceptions

--- src/main/java/com/playko/projectManagement/infrastructure/exceptionhandler/ControllerAdvisor.java
+++ src/main/java/com/playko/projectManagement/infrastructure/exceptionhandler/ControllerAdvisor.java
@@ -27,6 +27,8 @@ import com.playko.projectManagement.infrastructure.exception.UserAlreadyRestrict
 import com.playko.projectManagement.infrastructure.exception.UserNotFoundException;
 import com.playko.projectManagement.infrastructure.exception.UserNotInTeamException;
 import com.playko.projectManagement.infrastructure.exception.UserNotRestrictedException;
+import com.playko.projectManagement.shared.constants.ExceptionMessages;
+import com.playko.projectManagement.shared.constants.MailMessages;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
@@ -42,33 +44,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import static com.playko.projectManagement.shared.constants.Exceptions.BOARD_COLUMN_NOT_FOUND_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.BOARD_NOT_FOUND_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.DATA_NOT_FOUND_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.EMAIL_NOT_SEND_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.EMPTY_TEAM_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.FILE_NOT_FOUND_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.INVALID_BOARD_OPERATION_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.INVALID_KEYWORD_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.INVALID_PROJECT_STATE_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.INVALID_RESTRICTION_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.INVALID_TASK_STATE_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.PROJECTS_NOT_FOUND_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.PROJECT_ALREADY_EXISTS_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.PROJECT_NOT_FOUND_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.RESPONSE_MESSAGE_KEY;
-import static com.playko.projectManagement.shared.constants.Exceptions.ROLE_NOT_FOUND_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.SUB_TASK_NOT_FOUND_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.TASK_NOT_FOUND_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.TEAM_NOT_FOUND_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.UNAUTHORIZED_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.USERS_NOT_FOUND_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.USER_ALREADY_EXISTS_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.USER_ALREADY_IN_TEAM_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.USER_ALREADY_RESTRICTED_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.USER_NOT_FOUND_IN_TEAM_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.USER_NOT_FOUND_MESSAGE;
-import static com.playko.projectManagement.shared.constants.Exceptions.USER_NOT_RESTRICTED_MESSAGE;
+
 
 @ControllerAdvice
 public class ControllerAdvisor {
@@ -116,165 +92,165 @@ public class ControllerAdvisor {
     public ResponseEntity<Map<String, String>> unauthorizedException(
             UnauthorizedException unauthorizedException) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, UNAUTHORIZED_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.UNAUTHORIZED_MESSAGE));
     }
 
     @ExceptionHandler(NoUsersFoundException.class)
     public ResponseEntity<Map<String, String>> noUsersFoundException(
             NoUsersFoundException noUsersFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, USERS_NOT_FOUND_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.USERS_NOT_FOUND_MESSAGE));
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Map<String, String>> userNotFoundException(
             UserNotFoundException userNotFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, USER_NOT_FOUND_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.USER_NOT_FOUND_MESSAGE));
     }
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<Map<String, String>> userAlreadyExistsException(
             UserAlreadyExistsException userAlreadyExistsException) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, USER_ALREADY_EXISTS_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.USER_ALREADY_EXISTS_MESSAGE));
     }
 
     @ExceptionHandler(TeamNotFoundException.class)
     public ResponseEntity<Map<String, String>> teamNotFoundException(
             TeamNotFoundException teamNotFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, TEAM_NOT_FOUND_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.TEAM_NOT_FOUND_MESSAGE));
     }
     @ExceptionHandler(UserNotInTeamException.class)
     public ResponseEntity<Map<String, String>> userNotInTeamException(
             UserNotInTeamException userNotInTeamException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, USER_NOT_FOUND_IN_TEAM_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.USER_NOT_FOUND_IN_TEAM_MESSAGE));
     }
     @ExceptionHandler(ProjectAlreadyExistsException.class)
     public ResponseEntity<Map<String, String>> projectAlreadyExistsException(
             ProjectAlreadyExistsException projectAlreadyExistsException) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, PROJECT_ALREADY_EXISTS_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.PROJECT_ALREADY_EXISTS_MESSAGE));
     }
     @ExceptionHandler(ProjectNotFoundException.class)
     public ResponseEntity<Map<String, String>> projectNotFoundException(
             ProjectNotFoundException projectNotFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, PROJECT_NOT_FOUND_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.PROJECT_NOT_FOUND_MESSAGE));
     }
     @ExceptionHandler(BoardColumnNotFoundException.class)
     public ResponseEntity<Map<String, String>> boardColumnNotFoundException(
             BoardColumnNotFoundException boardColumnNotFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, BOARD_COLUMN_NOT_FOUND_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.BOARD_COLUMN_NOT_FOUND_MESSAGE));
     }
     @ExceptionHandler(TaskNotFoundException.class)
     public ResponseEntity<Map<String, String>> taskNotFoundException(
             TaskNotFoundException taskNotFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, TASK_NOT_FOUND_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.TASK_NOT_FOUND_MESSAGE));
     }
     @ExceptionHandler(InvalidProjectStateException.class)
     public ResponseEntity<Map<String, String>> invalidProjectStateException(
             InvalidProjectStateException invalidProjectStateException) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, INVALID_PROJECT_STATE_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.INVALID_PROJECT_STATE_MESSAGE));
     }
     @ExceptionHandler(SubTaskNotFoundException.class)
     public ResponseEntity<Map<String, String>> subTaskNotFoundException(
             SubTaskNotFoundException subTaskNotFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, SUB_TASK_NOT_FOUND_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.SUB_TASK_NOT_FOUND_MESSAGE));
     }
     @ExceptionHandler(MessageNotSendException.class)
     public ResponseEntity<Map<String, String>> messageNotSendException(
             MessageNotSendException messageNotSendException) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, EMAIL_NOT_SEND_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, MailMessages.EMAIL_NOT_SEND_MESSAGE));
     }
     @ExceptionHandler(BoardNotFoundException.class)
     public ResponseEntity<Map<String, String>> boardNotFoundException(
             BoardNotFoundException boardNotFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, BOARD_NOT_FOUND_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.BOARD_NOT_FOUND_MESSAGE));
     }
     @ExceptionHandler(InvalidTaskStateException.class)
     public ResponseEntity<Map<String, String>> invalidTaskStateException(
             InvalidTaskStateException invalidTaskStateException) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, INVALID_TASK_STATE_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.INVALID_TASK_STATE_MESSAGE));
     }
     @ExceptionHandler(UserAlreadyRestrictedException.class)
     public ResponseEntity<Map<String, String>> userAlreadyRestrictedException(
             UserAlreadyRestrictedException userAlreadyRestrictedException) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, USER_ALREADY_RESTRICTED_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.USER_ALREADY_RESTRICTED_MESSAGE));
     }
     @ExceptionHandler(UserNotRestrictedException.class)
     public ResponseEntity<Map<String, String>> userNotRestrictedException(
             UserNotRestrictedException userNotRestrictedException) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, USER_NOT_RESTRICTED_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.USER_NOT_RESTRICTED_MESSAGE));
     }
     @ExceptionHandler(ProjectsNotFoundException.class)
     public ResponseEntity<Map<String, String>> projectsNotFoundException(
             ProjectsNotFoundException projectsNotFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, PROJECTS_NOT_FOUND_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.PROJECTS_NOT_FOUND_MESSAGE));
     }
     @ExceptionHandler(StatsNotFoundException.class)
     public ResponseEntity<Map<String, String>> statsNotFoundException(
             StatsNotFoundException statsNotFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, statsNotFoundException.getMessage()));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, statsNotFoundException.getMessage()));
     }
     @ExceptionHandler(InvalidKeywordException.class)
     public ResponseEntity<Map<String, String>> invalidKeywordException(
             InvalidKeywordException invalidKeywordException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, INVALID_KEYWORD_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.INVALID_KEYWORD_MESSAGE));
     }
     @ExceptionHandler(FileNotFoundException.class)
     public ResponseEntity<Map<String, String>> fileNotFoundException(
             FileNotFoundException fileNotFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, FILE_NOT_FOUND_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.FILE_NOT_FOUND_MESSAGE));
     }
     @ExceptionHandler(RoleNotFoundException.class)
     public ResponseEntity<Map<String, String>> roleNotFoundException(
             RoleNotFoundException roleNotFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, ROLE_NOT_FOUND_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.ROLE_NOT_FOUND_MESSAGE));
     }
     @ExceptionHandler(UserAlreadyInTeamException.class)
     public ResponseEntity<Map<String, String>> userAlreadyInTeamException(
             UserAlreadyInTeamException userAlreadyInTeamException) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, USER_ALREADY_IN_TEAM_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.USER_ALREADY_IN_TEAM_MESSAGE));
     }
     @ExceptionHandler(EmptyTeamException.class)
     public ResponseEntity<Map<String, String>> emptyTeamException(
             EmptyTeamException emptyTeamException) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, EMPTY_TEAM_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.EMPTY_TEAM_MESSAGE));
     }
     @ExceptionHandler(InvalidRestrictionException.class)
     public ResponseEntity<Map<String, String>> invalidRestrictionException(
             InvalidRestrictionException invalidRestrictionException) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, INVALID_RESTRICTION_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.INVALID_RESTRICTION_MESSAGE));
     }
     @ExceptionHandler(InvalidBoardOperationException.class)
     public ResponseEntity<Map<String, String>> invalidBoardOperationException(
             InvalidBoardOperationException invalidBoardOperationException) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, INVALID_BOARD_OPERATION_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.INVALID_BOARD_OPERATION_MESSAGE));
     }
     @ExceptionHandler(DataNotFoundException.class)
     public ResponseEntity<Map<String, String>> dataNotFoundException(
             DataNotFoundException dataNotFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Collections.singletonMap(RESPONSE_MESSAGE_KEY, DATA_NOT_FOUND_MESSAGE));
+                .body(Collections.singletonMap(ExceptionMessages.RESPONSE_MESSAGE_KEY, ExceptionMessages.DATA_NOT_FOUND_MESSAGE));
     }
 }
